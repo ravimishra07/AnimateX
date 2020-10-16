5f699a31367d8739f6132aae6b0999a2d09aafff
ravi Mishra <ravimishra1017.rm@gmail.com>
2020-10-16 09:40:49 +0530

    Added bottom up cell transition animation

--- AnimateX/TransitionViewController.swift
+++ AnimateX/TransitionViewController.swift
@@ -9,21 +9,26 @@ import UIKit
 
 class TransitionViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
-    let dataSource = ["HomeLander","Queen Maeve","StarLight","A-Train","The Deep","Translucent","Black Noir","The Lamplighter","Stromfront","Kimiko","The Mesmerizer"]
+    let dataSource = ["HomeLander","Queen Maeve","StarLight","A-Train","The Deep","Translucent","Black Noir","The Lamplighter","Stromfront","Kimiko","The Mesmerizer","HomeLander","Queen Maeve","StarLight","A-Train","The Deep","Translucent","Black Noir","The Lamplighter","Stromfront","Kimiko","The Mesmerizer"]
+    var cellTranstaion:CGFloat = 0.0
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.dataSource = self
         self.tableView.delegate = self
+        cellTranstaion = self.view.bounds.height
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()+0.4) {
+            self.tableView.reloadData()
+        }
 
         // Do any additional setup after loading the view.
     }
     override func viewWillAppear(_ animated: Bool) {
-        self.view.transform = CGAffineTransform(translationX: 0, y: self.view.frame.height)
-        UIView.animateKeyframes(withDuration: 0.8, delay: 0, options: []) {
-            self.view.transform = .identity
-        } completion: { (isComplete) in
-            
-        }
+       // self.tableView.transform = CGAffineTransform(translationX: 0, y: self.view.frame.height)
+//        UIView.animateKeyframes(withDuration: 0.8, delay: 0, options: []) {
+//            self.view.transform = .identity
+//        } completion: { (isComplete) in
+//
+//        }
 
     }
 }
@@ -40,5 +45,16 @@ extension TransitionViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         90
     }
-    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cellTranstaion = (self.view.bounds.height - cell.contentView.bounds.height*CGFloat((indexPath.row+1)))
+        cell.transform = CGAffineTransform(translationX: 0, y: self.cellTranstaion)
+
+        UIView.animate(withDuration: 0.6) {
+            cell.transform = .identity//CGAffineTransform(translationX: 0, y: self.cellTranstaion)
+
+        } completion: { (isTrue) in
+            
+        }
+
+    }
 }
