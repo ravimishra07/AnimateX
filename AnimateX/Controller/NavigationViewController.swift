ce99738019d8fcc817a3919ebf6c0a6a3f997682
ravi Mishra <ravimishra1017.rm@gmail.com>
2020-10-26 00:59:44 +0530

    Added side menu animation using transform

--- AnimateX/Controller/NavigationViewController.swift
+++ AnimateX/Controller/NavigationViewController.swift
@@ -33,23 +33,35 @@ class NavigationViewController: UIViewController {
     @IBOutlet weak var sideMenuBtn5BottomContraint:NSLayoutConstraint!
     
     //MARK:- Constants
-    let ANIMATION_DURATION:TimeInterval = 0.8
-    let DELAY_DURATION:TimeInterval = 0.2
-    let DAMPING_CONSTANT:CGFloat = 0.35
-    
+    private let ANIMATION_DURATION:TimeInterval = 0.8
+    private let DELAY_DURATION:TimeInterval = 0.2
+    private let DAMPING_CONSTANT:CGFloat = 0.35
+    private let DAMPING_CONSTANT_SIDE_MENU:CGFloat = 0.7
+
+    private let SIDE_MENU_BOTTOM_CONSTANT:CGFloat = 8
+    private let SIDE_MENU_BOTTOM_INITIAL_CONSTANT:CGFloat = 38
+
     //MARK:- Variables
-    var isMenuShown = false
-    var baseBottomContraint:CGFloat = 40
+    private var isMenuShown = false
+    private var isBottomShown = false
+    private var baseBottomContraint:CGFloat = 40
  
     //MARK:- Lifecycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
         self.menuBtn3.transform = CGAffineTransform(rotationAngle: (.pi/4))
         initaliseViews()
+        //hideSideMenu(isHidden: true)
+        self.sideMenuBtn1.alpha = 0
+        self.sideMenuBtn2.alpha = 0
+        self.sideMenuBtn3.alpha = 0
+        self.sideMenuBtn4.alpha = 0
+        self.sideMenuBtn5.alpha = 0
+        sideMenuInitialState2()
     }
    
     //MARK:- ViewController's Functions
-    func initaliseViews(){
+   private func initaliseViews(){
         menuBtn1BottomContraint.constant = CGFloat(-(40+baseBottomContraint))
         menuBtn2BottomContraint.constant = CGFloat(-(50+baseBottomContraint))
         menuBtn4BottomContraint.constant = CGFloat(-(50+baseBottomContraint))
@@ -61,7 +73,7 @@ class NavigationViewController: UIViewController {
         }
     }
     
-    func animateView(){
+    private func animateView(){
         menuBtn1BottomContraint.constant = 0
         menuBtn2BottomContraint.constant = 0
         menuBtn4BottomContraint.constant = 0
@@ -73,19 +85,110 @@ class NavigationViewController: UIViewController {
         }
     }
     
+//    private func hideSideMenu(isHidden: Bool){
+//        sideMenuBtn1.isHidden = isHidden
+//        sideMenuBtn2.isHidden = isHidden
+//        sideMenuBtn3.isHidden = isHidden
+//        sideMenuBtn4.isHidden = isHidden
+//        sideMenuBtn5.isHidden = isHidden
+//    }
+//    private func hideBottomMenu(isHidden: Bool){
+//        menuBtn1.isHidden = isHidden
+//        menuBtn2.isHidden = isHidden
+//        menuBtn3.isHidden = isHidden
+//        menuBtn4.isHidden = isHidden
+//        menuBtn5.isHidden = isHidden
+//    }
+//
+    private func sideMenuInitialState(){
+        sideMenuBtn1BottomContraint.constant = 52
+        sideMenuBtn2BottomContraint.constant = SIDE_MENU_BOTTOM_CONSTANT
+        sideMenuBtn3BottomContraint.constant = SIDE_MENU_BOTTOM_CONSTANT
+        sideMenuBtn4BottomContraint.constant = SIDE_MENU_BOTTOM_CONSTANT
+        sideMenuBtn5BottomContraint.constant = SIDE_MENU_BOTTOM_CONSTANT
+        UIView.animate(withDuration: ANIMATION_DURATION, delay: 0.1, usingSpringWithDamping: DAMPING_CONSTANT, initialSpringVelocity: 0.5, options: [.curveEaseOut]) {
+            self.view.layoutIfNeeded()
+        } completion: { (isTrue) in
+            
+        }
+    }
+    
+    /// side menu postioned using constaints
+    private func sideMenuFinalState(){
+        sideMenuBtn1BottomContraint.constant = 32
+        sideMenuBtn2BottomContraint.constant = SIDE_MENU_BOTTOM_INITIAL_CONSTANT
+        sideMenuBtn3BottomContraint.constant = SIDE_MENU_BOTTOM_INITIAL_CONSTANT
+        sideMenuBtn4BottomContraint.constant = SIDE_MENU_BOTTOM_INITIAL_CONSTANT
+        sideMenuBtn5BottomContraint.constant = SIDE_MENU_BOTTOM_INITIAL_CONSTANT
+        UIView.animate(withDuration: ANIMATION_DURATION, delay: 0.1, usingSpringWithDamping: DAMPING_CONSTANT_SIDE_MENU, initialSpringVelocity: 0.5, options: [.curveEaseOut]) {
+            self.view.layoutIfNeeded()
+        } completion: { (isTrue) in
+            
+        }
+    }
+    /// side menu postioned using transform
+    private func sideMenuInitialState2(){
+        
+        UIView.animate(withDuration: ANIMATION_DURATION, delay: 0.1, usingSpringWithDamping: DAMPING_CONSTANT_SIDE_MENU, initialSpringVelocity: 0.5, options: [.curveEaseOut]) {
+            self.sideMenuBtn1.transform = .identity
+            self.sideMenuBtn2.transform = .identity
+            self.sideMenuBtn3.transform = .identity
+            self.sideMenuBtn4.transform = .identity
+            self.sideMenuBtn5.transform = .identity
+            self.sideMenuBtn1.alpha = 0
+            self.sideMenuBtn2.alpha = 0
+            self.sideMenuBtn3.alpha = 0
+            self.sideMenuBtn4.alpha = 0
+            self.sideMenuBtn5.alpha = 0
+
+            
+        } completion: { (isTrue) in
+           // self.hideSideMenu(isHidden: true)
+        }
+    }
+    private func sideMenuFinalState2(){
+        
+        UIView.animate(withDuration: ANIMATION_DURATION, delay: 0.1, usingSpringWithDamping: DAMPING_CONSTANT, initialSpringVelocity: 0.5, options: [.curveEaseOut]) {
+            self.sideMenuBtn1.transform = CGAffineTransform(translationX: 0, y: -self.SIDE_MENU_BOTTOM_INITIAL_CONSTANT/2)
+            self.sideMenuBtn2.transform = CGAffineTransform(translationX: 0, y: -(self.SIDE_MENU_BOTTOM_INITIAL_CONSTANT*2)+12)
+            self.sideMenuBtn3.transform = CGAffineTransform(translationX: 0, y: -(self.SIDE_MENU_BOTTOM_INITIAL_CONSTANT*3)+12)
+            self.sideMenuBtn4.transform = CGAffineTransform(translationX: 0, y: -(self.SIDE_MENU_BOTTOM_INITIAL_CONSTANT*4)+12)
+            self.sideMenuBtn5.transform = CGAffineTransform(translationX: 0, y: -(self.SIDE_MENU_BOTTOM_INITIAL_CONSTANT*5)+12)
+            self.sideMenuBtn1.alpha = 1
+            self.sideMenuBtn2.alpha = 2
+            self.sideMenuBtn3.alpha = 3
+            self.sideMenuBtn4.alpha = 4
+            self.sideMenuBtn5.alpha = 5
+        } completion: { (isTrue) in
+            
+        }
+    }
+    
     //MARK:- Actions
     @IBAction func crossTapped(_ sender: UIButton){
         if isMenuShown{
             self.initaliseViews()
+           // hideSideMenu(isHidden: false)
+
             UIView.animate(withDuration: 0.3) {
                 self.menuBtn3.transform = CGAffineTransform(rotationAngle: (.pi/4))
             }
         }else{
             self.animateView()
+           // hideSideMenu(isHidden: true)
             UIView.animate(withDuration: 0.3) {
                 self.menuBtn3.transform = .identity
             }
         }
         isMenuShown = !isMenuShown
+    }
+    
+    @IBAction func sideMenuTapped(_ sender: UIButton){
+        if isBottomShown{
+            self.sideMenuInitialState2()
+        }else{
+            self.sideMenuFinalState2()
+        }
+        isBottomShown = !isBottomShown
     }
 }
